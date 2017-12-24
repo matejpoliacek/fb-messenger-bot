fdf9f175673b0d2ae0f140c9591ed4b648c46f7b
Matej Poliacek <poliacek.matej@gmail.com>
2017-12-24 21:42:45 +0000

    db script change

--- db_script.py
+++ db_script.py
@@ -26,9 +26,9 @@ def db_close(cur, conn):
 def check_fbID(in_id):
 	conn = db_connect()
 	cur = conn.cursor()
-	cur.execute('SELECT EXISTS (SELECT true FROM userdb WHERE fb_id = %s LIMIT 1);', (in_id,))
+	cur.execute('SELECT COUNT (fb_id) FROM userdb WHERE fb_id = %s;', (in_id,))
 	found = cur.fetchall()
-	if found[0] == 'true':
+	if found[0] == 0:
 		db_close(cur, conn)
 		return True
 	else:
