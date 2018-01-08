87f3037fa68ddb310cf51453ab5b91189f6cc3b0
Matej Poliacek <poliacek.matej@gmail.com>
2018-01-08 22:20:00 +0000

    changed time of the scheduled job

--- clock.py
+++ clock.py
@@ -12,7 +12,7 @@ log.addHandler(h)
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=7)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=6)
 def scheduled_job():
 	print('This is a morning job')
 	weather_msg()
