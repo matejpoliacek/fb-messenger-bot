4b5db1ba593ea8b45f8f04238b05ab581c98d918
Matej Poliacek <poliacek.matej@gmail.com>
2018-03-14 21:50:50 +0000

    moved the scheduled behaviour from clock to a separate script to be run by heroku scheduler

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn app:app --log-file=-
-clock: python clock.py
+
---
+++ sched_script.py
@@ -0,0 +1,7 @@
+from msgs import weather_msg
+import sys
+
+print('This is a morning job')
+sys.stdout.flush()
+weather_msg()
+
